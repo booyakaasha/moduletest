709b783bf90d0b1af2cccb46fbb39bdbf207c116
Hello <kaawaabaanga@gmail.com>
2024-09-25 11:16:04 +0300

    first

---
+++ internal/storage/storage.go
@@ -0,0 +1,38 @@
+package storage
+
+import (
+	"fmt"
+
+	"github.com/booyakaasha/module/internal/storage/file"
+	"github.com/google/uuid"
+)
+
+type Storage struct {
+	Files map[uuid.UUID]*file.File
+}
+
+func NewStorage() *Storage { //указатель так захотеось
+	return &Storage{
+		Files: make(map[uuid.UUID]*file.File),
+	}
+
+}
+
+func (s *Storage) Upload(filename string, blob []byte) (*file.File, error) {
+	newFile, err := file.NewFile(filename, blob)
+	if err != nil {
+		return nil, err
+	}
+	s.Files[newFile.ID] = newFile
+
+	return newFile, nil
+}
+
+func (s *Storage) GetByID(fileID uuid.UUID) (*file.File, error) {
+	foundFile, ok := s.Files[fileID]
+	if !ok {
+		return nil, fmt.Errorf("file %v not found", fileID)
+	}
+
+	return foundFile, nil
+}
---
+++ pkg/storage/storage.go
@@ -0,0 +1,7 @@
+package storage
+
+import "github.com/booyakaasha/module/internal/storage"
+
+func NewStorage() *storage.Storage {
+	return storage.NewStorage()
+}
